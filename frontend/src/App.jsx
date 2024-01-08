ce46a77c49c171cb0f1cf87e150b915e16b0d7a3
abrar-khan-alvi <abrar1khan2@gmail.com>
2024-01-08 17:03:20 +0600

    sheeraaa

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import Adminpanel from "./AdminPanel/Adminpanel";
 const App = () => {
   return (
+
     <Adminpanel/>
     
   );
 };
 
 export default App;
+    //sheraa
