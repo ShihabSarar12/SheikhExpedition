edbe0265133ad1cfa1a0c2a62f3d03d80d73d665
abrar-khan-alvi <abrar1khan2@gmail.com>
2024-01-08 16:53:27 +0600

    file changed

---
+++ frontend/src/AdminPanel/Adminpanel.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar/Navbar";
+import Dashboard from "./Dashboard/Dashboard";
+import Projects from "./Project/Project";
+import Settings from "./Settings/Settings";
+import Blog from "./Blog/Blog";
+import Member from "./Member/Member";
+import Help from "./Help/Help";
+
+const Adminpanel = () => {
+    return (
+        <Router>
+          <div className="flex h-screen">
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />}/>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/blogs" element={<Blog/>}/>
+              <Route path="/team-members" element={<Member/>} />
+              <Route path="/help" element={<Help />} />
+              <Route path="/settings" element={<Settings/>}/>
+            </Routes>
+          </div>
+        </Router>
+      );
+};
+
+export default Adminpanel;
