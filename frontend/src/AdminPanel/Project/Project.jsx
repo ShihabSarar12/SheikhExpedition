d4bc66e655bbbcb20594aace0a584bc6c1bc2d21
abrar-khan-alvi <abrar1khan2@gmail.com>
2024-01-08 21:11:50 +0600

    added

--- frontend/src/AdminPanel/Project/Project.jsx
+++ frontend/src/AdminPanel/Project/Project.jsx
@@ -1,30 +1,29 @@
 import React, { useState, useEffect } from "react";
 import ProjectItem from "./ProjectItem";
-import pj from '../Others/project.json';
 
 const Projects = () => {
-//   const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState([]);
 
-//   useEffect(() => {
-//     const fetchProjects = async () => {
-//       try {
-//         const response = await fetch("../../public/project.json");
-//         const data = await response.json();
-//         setProjects(data);
-//       } catch (error) {
-//         console.error("Error fetching projects:", error);
-//       }
-//     };
+  useEffect(() => {
+    const fetchProjects = async () => {
+      try {
+        const response = await fetch('http://localhost:8080/projects');
+        const data = await response.json();
+        setProjects(data);
+      } catch (error) {
+        console.error("Error fetching projects:", error);
+      }
+    };
 
-//     fetchProjects();
-//   }, []);
+    fetchProjects();
+  }, []);
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="text-3xl font-bold mb-4">Projects Page</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {pj.map((project, index) => (
-          <ProjectItem key={index} project={project} />
+        {projects.map((project) => (
+          <ProjectItem key={project.ProjectID} project={project} />
         ))}
       </div>
     </div>
